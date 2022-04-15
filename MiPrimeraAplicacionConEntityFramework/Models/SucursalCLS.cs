b48f5ae38d2f7cefc242467eed520c2b12646b05
Eduardo Aguilar <aheduardo5@gmail.com>
2022-04-15 17:49:17 -0500

    Actualizacion en sucursal. Se crea formulario con validaciones para agregar sucursal

--- MiPrimeraAplicacionConEntityFramework/Models/SucursalCLS.cs
+++ MiPrimeraAplicacionConEntityFramework/Models/SucursalCLS.cs
@@ -11,16 +11,27 @@ namespace MiPrimeraAplicacionConEntityFramework.Models
         [Display(Name = "Id Sucursal")]
         public int iidsucursal { get; set; }
         [Display(Name = "Nombre Sucursal")]
+        [Required]
+        [StringLength(100, ErrorMessage = "No se permiten mas de 100 caracteres")]
         public string nombre { get; set; }
-       
+        [Display(Name = "Direccion sucursal")]
+        [Required]
+        [StringLength(200, ErrorMessage = "No se permiten mas de 200 caracteres")]
         public string direccion { get; set; }
-        [Display(Name ="Telefono Sucursal")]
+        [Display(Name = "Telefono Sucursal")]
+        [Required]
+        [StringLength(10,ErrorMessage ="No se permiten mas de 10 caracteres")]
         public string telefono { get; set; }
-        [Display(Name ="Email Sucursal")]
+        [Display(Name = "Email Sucursal")]
+        [Required]
+        [StringLength(100,ErrorMessage ="No se permiten mas de 100 caracteres")]
+        [EmailAddress(ErrorMessage ="Ingrese un email valido")]
         public string email { get; set; }
-
+        [Display(Name ="Fecha apertura")]
+        [Required(ErrorMessage ="Ingrese una fecha")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString ="{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime fechaApertura { get; set; }
-
         public int habilitado { get; set; }
     }
 }
